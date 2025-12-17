4d255613743b91b97d95beb84312ea885869194c
Igor Araújo <igoritoaraujo223@gmail.com>
2025-12-17 12:06:54 -0300

    save

---
+++ aula06.js
@@ -0,0 +1,10 @@
+// If e Else
+let idade = 18;
+
+if(idade >= 18) {
+        //código se for verdadeiro
+    console.log("Você é maior de idade")
+} else {
+    //código se for falso
+    console.log("Você é menor de idade")
+}
---
+++ aula07.js
@@ -0,0 +1,11 @@
+// else if
+
+let nota = 4;
+
+if(nota >= 7) {
+    console.log("Aprovado");
+} else if (nota >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
