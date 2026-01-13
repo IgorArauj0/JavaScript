f166677b8a7a486fd72a231db8b938d25d6c2417
Igor Araújo <igoritoaraujo223@gmail.com>
2026-01-13 16:25:55 -0300

    save

--- Aula18/script.js
+++ Aula18/script.js
@@ -1,37 +1,35 @@
 // Função responsável por processar a pré-inscrição do aluno
 function processarInscricao() {
-
-    // Busca no HTML o valor digitado no campo de nome
+ 
+    //Busca no HTML o VALOR digitado no input|campo nome
     const nome = document.querySelector('#nomeAluno').value;
 
-    // Busca no HTML o curso selecionado pelo usuário
+    //Busca no HTML o CURSO selecionado pelo usuário
     const curso = document.querySelector('#curso').value;
 
-    // Busca no HTML o turno selecionado pelo usuário
+    //Busca no HTML o TURNO selecionado pelo usuário
     const turno = document.querySelector('#turno').value;
 
-    // Seleciona o elemento onde a mensagem final será exibida
+    //Seleciona o elemento onde a mensagem final será exibida.
     const resultado = document.querySelector('#resultado');
+ 
+    //trim() remove espaços em branco antes e depois do texto
+    //verifica se algum campo está vazio e emite o alerta|mensagem resultado
+     if (nome.trim() === '' || curso === '' || turno === '') {
+       
+        //Exibe uma mensagem de erro para o usuário
+        resultado.innerText = 'Por favor, preencha todos os campos para continuar';
 
-    // Verifica se algum campo está vazio
-    // trim() remove espaços em branco antes e depois do texto
-    if (nome.trim() === '' || curso === '' || turno === '') {
-
-        // Exibe uma mensagem de erro para o usuário
-        resultado.innerText = 'Por favor, preencha todos os campos para continuar.';
-
-        // Interrompe a execução da função
+        //Interrompe a execução da função
         return;
-    }
+     }
 
-    // Caso todos os campos estejam preenchidos,
-    // cria uma mensagem institucional personalizada
+     //caso todos os campos estejam preenchidos,
+     // Cria uma mensagem institucional personalizada
     resultado.innerText = 
-        `Olá, ${nome}!
-        
-        Sua pré-inscrição no curso de ${curso}, 
-        no turno da ${turno}, foi registrada com sucesso.
+    
+    `Olá, ${nome}!
+     Sua pré-inscrição no curso de ${curso}, no turno da ${turno}, foi registrada com sucesso.
+     Em breve, nossa equipe entrará em contato para dar continuidade ao processo seletivo.`; 
 
-        Em breve, nossa equipe entrará em contato
-        para dar continuidade ao processo seletivo.`;
 }
